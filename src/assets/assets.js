16fb3dcb7b41eb7e70606349dba522029ae1a0d4
gauravdubey0011 <dubeygaurav198@gmail.com>
2025-08-25 08:15:43 +0530

    Initial Commit

---
+++ src/assets/assets.js
@@ -0,0 +1,101 @@
+import logo from './logo.svg'
+import logo_dark from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.png'
+import brand_img from './brand_img.png'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+import profile_img_1 from './profile_img_1.png'
+import profile_img_2 from './profile_img_2.png'
+import profile_img_3 from './profile_img_3.png'
+
+export const assets = {
+    logo,
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+    {
+      title: "Drawer Heaven",
+      price: "Rs 10000",
+      location: "Gurugram",
+      image: project_img_1
+    },
+    {
+      title: "Bedroom",
+      price: "Rs 50,000",
+      location: "Mumbai",
+      image: project_img_2
+    },
+    {
+      title: "Fall Ceiling",
+      price: "Rs 20,000",
+      location: "Noida",
+      image: project_img_3
+    },
+    {
+      title: "Drawing Room",
+      price: "Rs 30,000",
+      location: "Banglore",
+      image: project_img_4
+    },
+    {
+      title: "Almirah",
+      price: "Rs 60,000",
+      location: "Gurugram",
+      image: project_img_5
+    },
+    {
+      title: "Wall Serenity",
+      price: "Rs 90,000",
+      location: "Hyderabad",
+      image: project_img_6
+    },
+    
+  ];
+
+  export const testimonialsData = [
+    {
+        name: "Nikhil Singh",
+        title: "Marketing Manager",
+        image: profile_img_1,
+        alt: "Portrait of Donald Jackman",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect design. Their attention to detail and commitment to client satisfaction is unmatched."
+    },
+    {
+        name: "Shashwat Pandey",
+        title: "UI/UX Designer",
+        image: profile_img_2,
+        alt: "Portrait of Richard Nelson",
+        rating: 4,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect design. Their attention to detail and commitment to client satisfaction is unmatched."
+    },
+    {
+        name: "Ayush Mishra",
+        title: "Co-Founder",
+        image: profile_img_3,
+        alt: "Portrait of James Washington",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect design. Their attention to detail and commitment to client satisfaction is unmatched."
+    }
+];
